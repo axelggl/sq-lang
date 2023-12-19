d137dbb0da2f72f95f765dc186f9dbddedb87dee
maxel <axel.maerten@ynov.com>
2023-12-19 17:42:13 +0100

    join albums tracks

--- join-albums-tracks.sql
+++ join-albums-tracks.sql
@@ -2,4 +2,4 @@ SELECT albums.Title AlbumName, tracks.Name TrackName, tracks.Milliseconds
 FROM albums
 JOIN tracks ON albums.AlbumId = tracks.AlbumId
 ORDER BY tracks.Milliseconds ASC
-LIMIT 50
+LIMIT 50;
