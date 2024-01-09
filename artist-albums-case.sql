ea22f3cc748d59dc3303e8eb0fc5adc1fca4024d
maxel <axel.maerten@ynov.com>
2024-01-09 15:42:46 +0100

    artist albums case

---
+++ artist-albums-case.sql
@@ -0,0 +1,10 @@
+SELECT Name, count(albums.ArtistId) NbAlbums,
+    CASE
+        WHEN count(albums.ArtistId) = 1 THEN 'Unproductive'
+        WHEN count(albums.ArtistId) > 1 AND count(albums.ArtistId) < 10 THEN 'Productive'
+        ELSE 'Very Productive'
+    END AS IsProductive
+FROM artists
+JOIN albums ON artists.ArtistId = albums.ArtistId
+GROUP BY albums.ArtistId
+LIMIT 100;
