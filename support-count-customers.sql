51c8aad1cfb61e8cc2dcd776b689f78c5459b21a
maxel <axel.maerten@ynov.com>
2024-01-09 10:43:05 +0100

    support count customers

---
+++ support-count-customers.sql
@@ -0,0 +1,6 @@
+SELECT FirstName || ' ' || UPPER(LastName) FullName, COUNT(SupportRepId) AS NumberOfCustomers
+FROM employees
+JOIN customers ON employees.EmployeeId = customers.SupportRepId
+WHERE employees.Title = 'Sales Support Agent'
+GROUP BY EmployeeId, SupportRepId
+ORDER BY NumberOfCustomers ASC
