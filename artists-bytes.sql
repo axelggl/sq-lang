08543a04c0eea95d8b84757bec4b9c27af11c6da
maxel <axel.maerten@ynov.com>
2024-01-09 15:27:07 +0100

    artists bytes

--- artists-bytes.sql
+++ artists-bytes.sql
@@ -2,7 +2,7 @@ SELECT
     artists.Name AS ArtistName,
     albums.Title AS AlbumName,
     tracks.Name AS TrackName,
-    ROUND(tracks.Bytes / 1024 / 1024, 2) || ' MB' AS MegaBytes
+    ROUND(tracks.Bytes / 1024.0, 1024.0, 2) || ' MB' AS MegaBytes
 FROM artists
     JOIN albums ON artists.ArtistId = albums.ArtistId
     JOIN tracks ON albums.AlbumId = tracks.AlbumId
