fa5efebb6550628ce71f357949f91c6b80debba7
maxel <axel.maerten@ynov.com>
2024-01-09 10:25:30 +0100

    tracks count

--- tracks-count.sql
+++ tracks-count.sql
@@ -1,4 +1,4 @@
-SELECT genres.Name, COUNT(track.TrackId) AS NumberOfTracks
+SELECT genres.Name, COUNT(tracks.TrackId) AS NumberOfTracks
 FROM genres
 JOIN tracks ON genres.GenreId = tracks.GenreId
 GROUP BY genres.Name, tracks.GenreId;
